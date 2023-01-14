066112621483172d58ad981ec42ad8ef7fed92fa
wutsi <herve.tchepannou@wutsi.com>
2023-01-14 13:35:42 -0500

    log image download errors

--- lib/src/image.dart
+++ lib/src/image.dart
@@ -1,28 +1,36 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
 
+import 'logger.dart';
 import 'widget.dart';
 
 /// Descriptor of a [CachedNetworkImage]
 class SDUIImage extends SDUIWidget {
+  static final Logger _logger = LoggerFactory.create('SDUIImage');
   String? url;
   double? width;
   double? height;
   String? fit;
 
   @override
-  Widget toWidget(BuildContext context) => CachedNetworkImage(
-      key: id == null ? null : Key(id!),
-      imageUrl: url ?? '',
-      width: width,
-      height: height,
-      fit: toBoxFit(fit),
-      placeholder: (context, url) => Container(
-          alignment: Alignment.center,
-          width: 16.0,
-          height: 16.0,
-          child: const CircularProgressIndicator()),
-      errorWidget: (context, url, error) => const Icon(Icons.error));
+  Widget toWidget(BuildContext context) => url == null
+      ? Container()
+      : CachedNetworkImage(
+          key: id == null ? null : Key(id!),
+          imageUrl: url!,
+          width: width,
+          height: height,
+          fit: toBoxFit(fit),
+          placeholder: (context, url) => Container(
+              alignment: Alignment.center,
+              width: 16.0,
+              height: 16.0,
+              child: const CircularProgressIndicator()),
+          errorWidget: (context, url, error) {
+            _logger.w('Error while downloading $url', error);
+            return const Icon(Icons.error);
+          });
 
   @override
   SDUIWidget fromJson(Map<String, dynamic>? json) {
